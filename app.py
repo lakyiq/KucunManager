b9644dd7ece79c91404515e55ccbf47dcbe48cb9
qianyri <756320104@qq.com>
2020-11-01 11:02:17 +0800

    update
    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 import webbrowser
 import sqldb
 
@@ -13,11 +13,17 @@ def index():
 def other():
 	return render_template('records.html')
 
+@app.route('/count/')
+def count():
+	return render_template('count.html')
+
 @app.route('/data/goods/')
 def getgoods():
 	data = {"code": 0, "msg": ""}
-	name=request.args.get('name')
-	lst=sqldb.select_goods(name)
+	name=request.args.get('name') or ''
+	size = request.args.get('size') or ''
+	place=request.args.get('place') or ''
+	lst=sqldb.select_goods(name,size,place)
 	data['count']= len(lst)
 	data['data']=lst
 	return data
@@ -26,6 +32,18 @@ def getgoods():
 def getrecords():
 	data = {"code": 0, "msg": ""}
 	lst = sqldb.select_records()
+	data['count'] = len(lst)
+	data['data'] = lst
+	return data
+
+@app.route('/data/counts/')
+def getcounts():
+	place = request.args.get('place')
+	kind = request.args.get('kind')
+	date1= request.args.get('date1')
+	date2 = request.args.get('date2')
+	data = {"code": 0, "msg": ""}
+	lst = sqldb.count_goods(place, kind, date1, date2)
 	data['count'] = len(lst)
 	data['data'] = lst
 	return data
@@ -43,14 +61,28 @@ def op(kind):
 	elif kind=='add':
 		name=request.args.get('name')
 		size=request.args.get('size')
-		sqldb.insert_goods(name,size)
-	else:
+		factory = request.args.get('factory') or ''
+		place=request.args.get('place')
+		price=request.args.get('price') or 0
+		sqldb.insert_goods(name, size,factory,place,price)
+	elif kind=='del':
 		id=request.args.get('id')
 		sqldb.del_goods(id)
-	return render_template('goods.html')
+	else:
+		print('get it !')
+		print(request)
+		id = request.args.get('id')
+		name = request.args.get('name')
+		size = request.args.get('size')
+		place = request.args.get('place')
+		factory = request.args.get('factory') or ''
+		price = request.args.get('price') or 0
+		sqldb.update_goods(id,name,size,place,factory,price)
+	return jsonify()
 
-
-
+@app.route('/error/')
+def error():
+	return "500"
 
 if __name__ == '__main__':
 	webbrowser.open("http://127.0.0.1:5000/")
--- sqldb.py
+++ sqldb.py
@@ -3,11 +3,11 @@ import sqlite3
 def create(conn,cursor):
 	try:
 		cursor.execute(
-			'create table goods(id integer primary key autoincrement,name varchar(20) not null ,size varchar(20) not null,number float not null);')
+			'create table goods(id integer primary key autoincrement,name varchar(20) not null ,size varchar(20) not null,factory varchar(20),place varchar(10) not null,price float,number integer not null);')
 	except:
 		pass
 	try:
-		cursor.execute('create table records(id integer not null,time date not null,kind varchar(10) not null,change float not null,foreign key (id) references goods(id));')
+		cursor.execute('create table records(id integer not null,time date not null,kind varchar(10) not null,change integer not null,foreign key (id) references goods(id));')
 	except:
 		pass
 
@@ -19,16 +19,12 @@ def dict_factory(cursor, row):
 	return d
 
 
-def select_goods(name):
+def select_goods(name,size,place):
 	conn = sqlite3.connect('kucun.db')
 	conn.row_factory = dict_factory
 	cursor = conn.cursor()
 	create(conn,cursor)
-	sql='select * from goods '
-	if name:
-		sql+='where name like \'%'+name+'%\';'
-	else:
-		sql+=';'
+	sql='select * from goods where name like \'%'+name+'%\' and size like \'%'+size+'%\' and place like\'%'+place+'%\';'
 	cursor.execute(sql)
 	goods = cursor.fetchall()
 	cursor.close()
@@ -42,7 +38,7 @@ def select_records():
 	conn.row_factory = dict_factory
 	cursor = conn.cursor()
 	create(conn, cursor)
-	cursor.execute('select time,name,size,kind,change from records left join goods on records.id=goods.id order by time;')
+	cursor.execute('select time,name,size,place,kind,change from goods left join records on records.id=goods.id order by time desc ;')
 	records = cursor.fetchall()
 	cursor.close()
 	conn.commit()
@@ -50,12 +46,12 @@ def select_records():
 	return records
 
 
-def insert_goods(name, size):
+def insert_goods(name, size,factory,place,price):
 	conn = sqlite3.connect('kucun.db')
 	conn.row_factory = dict_factory
 	cursor = conn.cursor()
 	create(conn, cursor)
-	cursor.execute('insert into goods values(null,?,?,0);', (name, size,))
+	cursor.execute('insert into goods values(null,?,?,?,?,?,0);', (name, size,factory,place,price,))
 	cursor.close()
 	conn.commit()
 	conn.close()
@@ -65,7 +61,7 @@ def del_goods(id):
 	conn.row_factory = dict_factory
 	cursor = conn.cursor()
 	create(conn, cursor)
-	cursor.execute('delete from goods where id=?',(id,))
+	cursor.execute('delete from goods where id=?;',(id,))
 	cursor.close()
 	conn.commit()
 	conn.close()
@@ -76,13 +72,41 @@ def insert_records(id, kind ,change):
 	cursor = conn.cursor()
 	create(conn, cursor)
 	if kind==1:
-		cursor.execute('insert into records values(?,datetime(\'now\'),?,?);', (id, '入库',change,))
+		cursor.execute('insert into records values(?,datetime(\'now\',\'localtime\'),?,?);', (id, '入库',change,))
 		cursor.execute('update goods set number=number+? where id=?;', (change, id,))
 	else:
-		cursor.execute('insert into records values(?,datetime(\'now\'),?,?);', (id, '出库', change,))
+		cursor.execute('insert into records values(?,datetime(\'now\',\'localtime\'),?,?);', (id, '出库', change,))
 		cursor.execute('update goods set number=number-? where id=?;', (change, id,))
 	cursor.close()
 	conn.commit()
 	conn.close()
 
+def update_goods(id,name,size,place,factory,price):
+	conn = sqlite3.connect('kucun.db')
+	conn.row_factory = dict_factory
+	cursor = conn.cursor()
+	create(conn, cursor)
+	cursor.execute('update goods set name=? ,size=?,place=?,factory=?,price=? where id=?;',(name,size,place,factory,price,id,))
+	cursor.close()
+	conn.commit()
+	conn.close()
 
+def count_goods(place,kind,date1,date2):
+	conn = sqlite3.connect('kucun.db')
+	conn.row_factory = dict_factory
+	cursor = conn.cursor()
+	create(conn, cursor)
+	cursor.execute('select goods.id as id,name,size,place,factory,price,number,kind,sum(change) as sum_change,price*sum(change) as money from goods left join records on goods.id = records.id where place=? and kind=? and time >=? and time<=? group by kind,goods.id order by goods.id;',(place,kind,date1,date2),)
+	counts = cursor.fetchall()
+	cursor.close()
+	conn.commit()
+	conn.close()
+	return counts
+
+if __name__ == '__main__':
+	conn = sqlite3.connect('kucun.db')
+	cursor = conn.cursor()
+	create(conn, cursor)
+	cursor.close()
+	conn.commit()
+	conn.close()
